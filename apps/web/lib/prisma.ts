fc3e095f6cd19beec5cc0b1d90059025d286f807
Chandan Swarnakar <itschandan1372@gmail.com>
2026-07-08 20:11:09 +0530

    feat: implement singleton Prisma client with pg adapter support

--- apps/web/lib/prisma.ts
+++ apps/web/lib/prisma.ts
@@ -17,7 +17,7 @@ export function createPrismaClient() {
   }
 
   return new PrismaClient({
-    adapter: new PrismaPg({ connectionString })
+    adapter: new PrismaPg({ connectionString, pool: { max: 3 } })
   });
 }
 
